4c58ed8644a774c30f839ce6fedccf8f083aa355
Pavlov Artur <don.artur16084353@gmail.com>
2023-10-06 00:32:25 +0300

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/testovoe-zadanie-1/'
+    ? '/testovoe-zadanie-1-1/'
     : '/'
 })
